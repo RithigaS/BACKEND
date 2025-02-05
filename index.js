0f2aff3afa6158b503cf2c0a31d41e04b31d48df
RithigaS <rithigasadhasivam@gmail.com>
2025-02-05 19:30:05 +0530

    last commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://rithigas2023cse:rithi2006@cluster0.nst8v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Main_blog', {
+mongoose.connect('mongodb+srv://rithigas2023cse:rithi2006@cluster0.nst8v.mongodb.net/Main_Blog?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('Connected to MongoDB'))
